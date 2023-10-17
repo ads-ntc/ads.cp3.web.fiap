8f1e57fc434131be5fb06fc97990e3aaae259218
Rminoro <114706933+Rminoro@users.noreply.github.com>
2023-10-17 01:27:19 +0000

    Código do produto sapato

---
+++ src/app/produtos/sapato/page.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import Link from "next/link"
+import "../../../components/styles/styles.css"
+
+import SapatoImg from "../../assets/sapato.jpg"
+
+export default function Sapato() {
+  return (
+    <>
+    <div className="container-produto">
+      <div className="descricao-produto">
+        <h1>Sapato</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus incidunt totam laborum tenetur, aliquam dolor temporibus nesciunt accusamus doloribus nemo quas ratione quos corporis est odio, repellendus reiciendis dignissimos.</p>
+        <p className="valor" >R$: 80,99</p>
+        <button>COMPRAR</button>
+        </div>
+        <figure>
+          <Image src={SapatoImg} width={400} height={400} alt="Tenis" />
+          <figcaption>Tenis</figcaption>
+        </figure>
+    </div>
+    </>
+  )
+}
