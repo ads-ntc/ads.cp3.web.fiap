ebd6d91f288d456ea0ef29933738e0064b033245
Rminoro <114706933+Rminoro@users.noreply.github.com>
2023-10-17 01:27:09 +0000

    Código do produto oculos

---
+++ src/app/produtos/oculos/page.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import "../../../components/styles/styles.css"
+
+import OculosImg from "../../assets/oculos.jpg"
+
+export default function Oculos() {
+  return (
+    <div className="container-produto">
+      <div className="descricao-produto">
+        <h1>Oculos</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus incidunt totam laborum tenetur, aliquam dolor temporibus nesciunt accusamus doloribus nemo quas ratione quos corporis est odio, repellendus reiciendis dignissimos.</p>
+        <p className="valor" >R$: 80,99</p>
+        <button>COMPRAR</button>
+        </div>
+        <figure>
+          <Image src={OculosImg} width={400} height={400} alt="Tenis" />
+          <figcaption>Tenis</figcaption>
+        </figure>
+    </div>
+  )
+}
