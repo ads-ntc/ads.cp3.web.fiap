c93af01407073ff028984b02a2edf1152afe93a8
Kaique Oliveira <126740092+kkoliveiraa@users.noreply.github.com>
2023-10-17 22:45:12 +0000

    Codigo do produto calca

---
+++ src/app/produtos/calca/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+import "../../../components/styles/styles.css"
+
+import CalcaImg from "../../assets/calca.jpg"
+
+export default function Calca() {
+  return (
+    <div className="container-produto">
+      <div className="descricao-produto">
+        <h1>Calça</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus incidunt totam laborum tenetur, aliquam dolor temporibus nesciunt accusamus doloribus nemo quas ratione quos corporis est odio, repellendus reiciendis dignissimos.</p>
+        <p className="valor" >R$: 80,99</p>
+        <button>COMPRAR</button>
+        </div>
+        <figure>
+          <Image src={CalcaImg} width={400} height={400} alt="Tenis" />
+          <figcaption>Calça</figcaption>
+        </figure>
+    </div>
+  )
+}
